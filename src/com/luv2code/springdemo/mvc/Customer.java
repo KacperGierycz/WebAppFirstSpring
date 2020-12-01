736f1ff41691fa063b7117f8ef67d7615bce909c
Kacper <kacpergierycz@gmail.com>
2020-12-01 17:34:44 +0100

    The regexd for postalCode

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -11,6 +14,13 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 
+	@Min(value=0, message="must be greater or equal then zero")
+	@Max(value=10, message="must be les then or equal to 10")
+	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -25,6 +35,22 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 
